de2b1fbd133fd945d431a0db11049a85aebe5b38
Daria Simenkova <17787045+7yon@users.noreply.github.com>
2025-11-25 08:15:08 +0100

    Initial Commit

---
+++ Places/PlacesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlacesApp.swift
+//  Places
+//
+//  Created by Daria Simenkova on 25.11.25.
+//
+
+import SwiftUI
+
+@main
+struct PlacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
