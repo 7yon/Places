431c610c0887aca2d64bc31e5fffc99a69b7811d
Daria Simenkova <17787045+7yon@users.noreply.github.com>
2025-11-29 11:55:36 +0100

    feat: introduce app coordinator

---
+++ Places/AppCoordinator.swift
@@ -0,0 +1,57 @@
+//
+//  AppCoordinator.swift
+//  Places
+//
+//  Created by Daria Simenkova on 29.11.25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Route: Hashable {
+    case placesList
+    case customLocation
+}
+
+enum Deeplink: Hashable {
+    case wikipedia(latitude: Double, longitude: Double)
+}
+
+@Observable
+class AppCoordinator {
+    var path = NavigationPath()
+
+    private let rootComponent: RootComponent
+
+    private let urlOpener: URLOpener
+
+    init(rootComponent: RootComponent,
+         urlOpener: URLOpener) {
+        self.rootComponent = rootComponent
+        self.urlOpener = urlOpener
+    }
+
+    func route(to route: Route) {
+        path.append(route)
+    }
+
+    @ViewBuilder
+    func view(for route: Route) -> some View {
+        switch route {
+        case .placesList:
+            rootComponent.listComponent.view
+        case .customLocation:
+            rootComponent.customLocation.view
+        }
+    }
+
+    func route(to deeplink: Deeplink) async -> Bool {
+        switch deeplink {
+        case .wikipedia(let latitude, let longitude):
+            guard let url = URL(string: "wikipedia://places?latitude=\(latitude)&longitude=\(longitude)") else {
+                return false
+            }
+            return await urlOpener.open(url)
+        }
+    }
+}
